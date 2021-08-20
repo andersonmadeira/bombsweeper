dbddd49780ea6445a46116be1fca4eedcb39c2b1
Anderson Madeira <anderson.madeira@smartmresearch.com>
2021-08-20 16:00:09 -0300

    chore: set default playable level

--- src/App.tsx
+++ src/App.tsx
@@ -2,5 +2,9 @@ import React from 'react'
 import { Grid } from './components'
 
 export const App: React.FC = () => {
-  return <Grid lines={12} columns={12} mines={20} />
+  return <Grid lines={10} columns={10} mines={5} />
 }
+
+// easy: 10, 10: 5
+// normal: 10, 10, 10
+// hard: 12, 12, 30
